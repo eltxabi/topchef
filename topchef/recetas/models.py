f9eab25b5d87c2cf76f175019c590c8e10be0a97
profesor <profesor@example.com>
2015-10-12 21:23:40 +0200

    model receta created

---
+++ topchef/recetas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Receta(models.Model):
+   nombre = models.CharField(max_length=30)
+   instrucciones = models.TextField()
+   fecha = models.DateTimeField()
+
