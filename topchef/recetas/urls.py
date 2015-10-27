d9021d5c31eb66e82eef695aebf534088b52de87
profesor <profesor@example.com>
2015-10-28 00:03:59 +0100

    auth system created

--- topchef/recetas/urls.py
+++ topchef/recetas/urls.py
@@ -4,4 +4,7 @@ from recetas import views
 urlpatterns = [
    url(r'^$', views.indice, name='indice'),
 	url(r'^(?P<receta_id>\d+)/$',views.detalle_receta, name='detalle_receta'),
+	 url(r'^registro$', views.registro, name='registro'),
+    url(r'^login$', views.loginpage, name='login'),
+    url(r'^logout$', views.logoutpage, name='logout'),	
 ]
--- topchef/recetas/views.py
+++ topchef/recetas/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import get_object_or_404,render
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
+from django.http import HttpResponseRedirect
 from recetas.models import Receta
 
 # Create your views here.
@@ -11,5 +14,31 @@ def detalle_receta(request,receta_id):
 	receta = get_object_or_404(Receta, pk = receta_id)
 	return render(request, 'recetas/detalle_receta.html', {'receta': receta })
 
+def registro(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			new_user = form.save()
+			return HttpResponseRedirect("/")
+	else:
+		form = UserCreationForm()
+	return render(request, "recetas/registro.html", {
+'form': form,})
 
+def loginpage(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(request.POST)
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username,password=password)
+		if user is not None:
+			login(request,user)
+			return HttpResponseRedirect("/")
+	else:
+		form = AuthenticationForm()
+	return render(request,'recetas/login.html', {'form': form,})  
+
+def logoutpage(request):
+	logout(request)
+	return HttpResponseRedirect("/")
 
